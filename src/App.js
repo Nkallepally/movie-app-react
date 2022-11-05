d44799fe1f5e257847412a09e86300c133e0963e
Nkallepally <kallepallynavya@gmail.com>
2022-11-05 13:49:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=343bec8f`;
+		// http://www.omdbapi.com/?i=tt3896198&apikey=343bec8f
+
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
